e03fd10091753c01d9605dacaf3c1c0071131002
kushalgowda2004 <157580506+kushalgowda2004@users.noreply.github.com>
2024-12-09 09:43:26 +0530

    Add files via upload

---
+++ sample.java
@@ -0,0 +1,10 @@
+public class sample{
+    public sample()
+    {
+    }
+    
+        public static void main(String[] args) {
+            System.out.println("hello world");
+            System.out.println("welcome to java");
+        }
+    }
